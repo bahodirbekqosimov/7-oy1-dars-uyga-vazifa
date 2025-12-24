6bdf7504f51109cba5e6e4745aff9b5258b21426
Bahodirbek <bahodirbek20090405@gmail.com>
2025-12-24 10:02:48 +0500

    full homework

--- 2-topshiriq/main/urls.py
+++ 2-topshiriq/main/urls.py
@@ -6,6 +6,7 @@ from . import views
 urlpatterns = [
 
     path("hello/",views.HelloView.as_view(),name="hello" ),
-    path("greet/",views.GreetView.as_view(),name="greet" )
-    
+    path("greet/",views.GreetView.as_view(),name="greet" ),
+    path("echo/",views.EchoView.as_view(), name='echo')
+
 ]
--- 2-topshiriq/main/views.py
+++ 2-topshiriq/main/views.py
@@ -9,23 +9,23 @@ class HelloView(APIView):
     def get(self,res:Request):
         return Response(self.data[0],status=200)
     
-    def post(self,req:Request):
-        self.data.append({"message":"Hello, API!"})
-        return Response(self.data,status=201)
+    # def post(self,req:Request):
+    #     self.data.append({"message":"Hello, API!"})
+    #     return Response(self.data,status=201)
     
-    def put(self,req:Request):
-        data = []
-        for obj in self.data:
-            obj['message'] = 'Hello world'
-            data.append(obj)
+    # def put(self,req:Request):
+    #     data = []
+    #     for obj in self.data:
+    #         obj['message'] = 'Hello world'
+    #         data.append(obj)
         
-        self.data = data
+    #     self.data = data
         
-        return Response(self.data,status=200)
-    def delete(self,req:Request):
-        self.data = []
+    #     return Response(self.data,status=200)
+    # def delete(self,req:Request):
+    #     self.data = []
         
-        return Response(self.data,status=200)
+    #     return Response(self.data,status=200)
     
     
 class GreetView(APIView):
@@ -42,7 +42,98 @@ class GreetView(APIView):
     
 
 
-            
-
     
+class EchoView(APIView):
+    # def get(self,req:Request):
+    #     data = {
+    #         "matn":req.data.get("text")
+    #     }
+    #     return Response(data,status=200)
+
+    def post(self,req:Request):
+        data = {
+            "matn":req.data.get("text")
+        }
+        return Response(data,status=200)
+
+    # def delete(self,req:Request):
+    #     data = {
+    #         "matn":req.data.get("text")
+    #     }
+    #     return Response(data,status=200)
+
+    # def put(self,req:Request):
+    #     data = {
+    #         "matn":req.data.get("text")
+    #     }
+    #     return Response(data,status=200)
+
+
+
+class Check_ageView(APIView):
+    # def get(self,req:Request):
+    #     age = req.data.get("age")
         
+    #     if age != None and int(age)<18:
+    #         return Response({"message":'Access denied'} , status=403)
+        
+    #     return Response({"message":"Access granted"} , status=200)
+    
+    def post(self,req:Request):
+        age = req.data.get("age")
+        
+        if age != None and  int(age)<18:
+            return Response({"message":'Access denied'} , status=403)
+        
+        return Response({"message":"Access granted"} , status=200)
+    
+    # def delete(self,req:Request):
+    #     age = req.data.get("age")
+        
+    #     if age != None and int(age)<18:
+    #         return Response({"message":'Access denied'} , status=403)
+        
+    #     return Response({"message":"Access granted"} , status=200)
+    
+    # def put(self,req:Request):
+    #     age = req.data.get("age")
+        
+    #     if age != None and  int(age)<18:
+    #         return Response({"message":'Access denied'} , status=403)
+        
+    #     return Response({"message":"Access granted"} , status=200)
+
+
+
+class RegisterView(APIView):
+    
+    # def get(self,req:Request):
+    #     if req.data.get("username"):
+    #         return Response({"messgae":'User registered'}, status=201)
+    #     return Response(status=400)
+    
+    def post(self,req:Request):
+        if req.data.get("username"):
+            return Response({"messgae":'User registered'}, status=201)
+        return Response(status=400)
+    
+    # def delete(self,req:Request):
+    #     if req.data.get("username"):
+    #         return Response({"messgae":'User registered'}, status=201)
+    #     return Response(status=400)
+
+    # def put(self,req:Request):
+    #     if req.data.get("username"):
+    #         return Response({"messgae":'User registered'}, status=201)
+    #     return Response(status=400)
+    
+    
+class SquareView(APIView):
+    def get(self,req:Request, number):
+        data = {
+            "number":number,
+            "square": number*number
+        }
+        return Response(data,status=200)
+
+
