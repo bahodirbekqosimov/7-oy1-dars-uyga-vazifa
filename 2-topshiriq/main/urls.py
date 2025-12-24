e72f6612bf6b20520f1b5e194253790c3451bdc9
Bahodirbek <bahodirbek20090405@gmail.com>
2025-12-24 10:22:47 +0500

    full homework

--- 2-topshiriq/main/urls.py
+++ 2-topshiriq/main/urls.py
@@ -7,6 +7,11 @@ urlpatterns = [
 
     path("hello/",views.HelloView.as_view(),name="hello" ),
     path("greet/",views.GreetView.as_view(),name="greet" ),
-    path("echo/",views.EchoView.as_view(), name='echo')
+    path("echo/",views.EchoView.as_view(), name='echo'),
+    path("check-age/", views.Check_ageView.as_view(),name = 'check-age'),
+    path("register/", views.RegisterView.as_view(), name = 'register'),
+    path("square/<int:number>/", views.SquareView.as_view(), name='square'),
+    
+    
 
 ]
